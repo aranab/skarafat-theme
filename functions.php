87611a51f985542085eeb76eb74065f8e3b3dbd8
Md. Arafat Rahman Rana <aranab.dba@gmail.com>
2018-10-25 05:15:42 +0600

    Removed npm container from assets

--- functions.php
+++ functions.php
@@ -122,9 +122,13 @@ add_action( 'widgets_init', 'skarafat_widgets_init' );
 function skarafat_scripts() {
 	wp_enqueue_style( 'skarafat-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'skarafat-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_style( 'skarafat-main', get_template_directory_uri() . '/assets/main.min.css', false, '0.1', 'all' );
 
-	wp_enqueue_script( 'skarafat-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'skarafat-js', get_template_directory_uri() . '/assets/bundle.min.js', array(), '0.1', true );
+
+	//wp_enqueue_script( 'skarafat-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+
+	//wp_enqueue_script( 'skarafat-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
